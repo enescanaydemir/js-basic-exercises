adc87a9f5659aa7f61ac3d0a4daed2d711cdf877
Enes Can Aydemir <enescan_aydemir@hotmail.com>
2022-11-06 02:36:10 +0300

    fix

--- gpa-calculator/app.js
+++ gpa-calculator/app.js
@@ -1,10 +1,8 @@
 // Soru3: Klavyeden girilen vize ve final notlarına göre öğrencinin dersi geçip geçmediğini söyleyen bir uygulama yazınız.
 // (Geçme notu: 50, vizenin %40'ı, finalin %60'ı geçerlidir.)
-const vize = parseInt(prompt("Vize notunuzu giriniz"));
-const final = parseInt(prompt("Final notunuzu giriniz"));
-const vizeOrt = ((vize * 40) / 100);
-const finalOrt = ((final * 60) / 100);
-const genelOrt = (finalOrt + vizeOrt);
+const vize = parseInt(prompt("Vize notunuzu giriniz", "50"));
+const final = parseInt(prompt("Final notunuzu giriniz", "50"));
+const genelOrt = vize * 0.4 + final * 0.6;
 
 console.log(`Not Ortalamanız: ${genelOrt}`);
 
